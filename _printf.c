fed2dbfa31224c2bf11949682987adca10d13f29
Holberton <holberton@Holbertons-iMac-2.local>
2019-10-26 11:27:56 -0500

    print length

--- _printf.c
+++ _printf.c
@@ -5,14 +5,15 @@ int _printf(const char *format, ...)
 {
 	int i = 0;
 
+/* recorrer format */
 	while (format[i])
 	{
 		if (format[i] == 92)
 		{
 			if (format[i + 1] == 32 || format[i + 1] == 92 || format[i + 1] == 37)
 			{
-				i++;
 				_putchar(format[i]);
+				i++;
 			}
 		}
 		_putchar(format[i]);
@@ -20,13 +21,4 @@ int _printf(const char *format, ...)
 	}
 	
 	return (i);
-}
-
-int main(void)
-{
-
-    _printf("Let's try to printf a simple sentence.\n");
-    printf("Let's try 1 to printf a simple sentence.\n");
-
-    return (0);
 }
